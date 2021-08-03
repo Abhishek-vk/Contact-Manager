b052db293152d141d8951d9f9b5433f0df5e9fcc
Abhishek <abhishek.vk100@gmail.com>
2021-08-04 00:27:24 +0530

    contact list complete

--- src/components/home/contactList.js
+++ src/components/home/contactList.js
@@ -1,17 +1,52 @@
+import { useEffect, useState } from "react";
 import "../css/contactList.css";
 
-const ContactBox = (props) => {
-    const { name } = props.contact;
-    return <div className="ps-2 py-3 contactBox">{name}</div>;
+const ContactBox = ({ contact, isActive, setActive, showActiveContact }) => {
+    const { name, index } = contact;
+    return (
+        <div
+            className={
+                isActive()
+                    ? "ps-2 py-3 contactBox activeContactBox"
+                    : "ps-2 py-3 contactBox"
+            }
+            onClick={() => {
+                setActive(index);
+                showActiveContact();
+            }}
+        >
+            {name}
+        </div>
+    );
 };
 
-export default function ContactList(props) {
+export default function ContactList({ list, total, showActiveContact }) {
+    const [isActiveList, setActiveList] = useState([]);
+
+    useEffect(() => {
+        setActiveList(new Array(total).fill(false));
+    }, [total]);
+
+    const setActive = (index) => {
+        let tempIsActive = [...isActiveList];
+        tempIsActive.fill(false);
+        tempIsActive[index] = true;
+        setActiveList(tempIsActive);
+    };
     return (
         <div id="contactList" className="h-100 col-auto pe-0">
-            {props.list.map((contactGroup) =>
+            {list.map((contactGroup) =>
                 !!contactGroup.length
-                    ? contactGroup.map((contact) => (
-                          <ContactBox key={contact.id} contact={contact} />
+                    ? contactGroup.map((contact, index) => (
+                          <ContactBox
+                              key={index}
+                              contact={contact}
+                              isActive={() => isActiveList[contact.index]}
+                              setActive={setActive}
+                              showActiveContact={() =>
+                                  showActiveContact(contact)
+                              }
+                          />
                       ))
                     : null
             )}
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,31 +3,46 @@ import { useState, useEffect } from "react";
 
 export default function Homepage() {
     const [contactList, setcontactList] = useState([]);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         fetch("http://localhost:8000/api/v1/contacts")
             .then((response) => response.json())
-            .then((result) => result.data)
+            .then((result) => {
+                // setActiveList(new Array(result.total).fill(false));
+                setTotal(result.total);
+                return result.data;
+            })
             .then((data) => {
-                let GroupedContacts = [];
+                let GroupedContacts = [],
+                    count = 0;
                 for (let index = 0; index < 27; index++) {
                     GroupedContacts.push([]);
                 }
-                console.log(GroupedContacts);
                 data.forEach((contact) => {
-                    GroupedContacts[contact.name.charCodeAt(0) - 64].push(
-                        contact
-                    );
+                    GroupedContacts[contact.name.charCodeAt(0) - 64].push({
+                        index: count,
+                        ...contact,
+                    });
+                    count++;
                 });
                 setcontactList(GroupedContacts);
             })
             .catch((err) => console.log(err));
     }, []);
 
+    const showActiveContact = (contact) => {
+        console.log(contact);
+    };
+
     return (
         <div className="overflow-hidden" style={{ flex: "1" }}>
-            <div class="row h-100">
-                <ContactList list={contactList} />
+            <div className="row h-100">
+                <ContactList
+                    total={total}
+                    list={contactList}
+                    showActiveContact={showActiveContact}
+                />
                 <div className="col p-0"></div>
             </div>
         </div>
